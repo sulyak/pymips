d3702d8826749324dacfde6540cc6d68bf9e56f6
Rayner Sulyak <raynersulyak@gmail.com>
2022-06-12 16:01:43 -0300

    revamp binary converter

--- main.py
+++ main.py
@@ -46,32 +46,8 @@ def main():
 
 
 def make_r(ins, args):
-    result = "000000"
-    args = list(map(lambda x: x if x in reg.keys() else int(x), args))
-
-    if ins == 'jr':
-        rs, = args
-        result += bin(reg[rs])[2:].rjust(5, '0')
-        result += "0" * 15
-        result += "001000"
-        return result
-
-    if ins in ['sll', 'srl']:
-        rd, rs, sa = args
-        result += "00000"
-        result += bin(reg.get(rs, rs))[2:].rjust(5, '0')
-        result += bin(reg.get(rd, rd))[2:].rjust(5, '0')
-        result += bin(reg.get(sa, sa))[2:].rjust(5, '0')
-
-        funct = {'sll': 0x0, 'srl': 0x2}
-        result += bin(funct[ins])[2:].rjust(6, '0')
-        return result
-
-    rd, rs, rt = args
-    result += bin(reg.get(rs, rs))[2:].rjust(5, '0')
-    result += bin(reg.get(rt, rt))[2:].rjust(5, '0')
-    result += bin(reg.get(rd, rd))[2:].rjust(5, '0')
-    result += "00000"
+    # TODO remove this
+    # args = list(map(lambda x: x if x in reg.keys() else int(x), args))
 
     funct = {
             "add": 0x20,
@@ -83,7 +59,29 @@ def make_r(ins, args):
             "srl": 0x2
             }
 
-    result += bin(funct[ins])[2:].rjust(6, '0')
+    # opcode is 0 for every r instruction
+    result = "000000"
+
+    if ins == 'jr':
+        rs, = args
+        result += int2bin(reg[rs], num_bits=5)
+        result += "0" * 15
+
+    elif ins in ['sll', 'srl']:
+        rd, rt, sa = args
+        result += "00000"
+        result += int2bin(reg.get(rt, rt), num_bits=5)
+        result += int2bin(reg.get(rd, rd), num_bits=5)
+        result += int2bin(reg.get(sa, sa), num_bits=5)
+
+    else:
+        rd, rs, rt = args
+        result += int2bin(reg.get(rs, rs), num_bits=5)
+        result += int2bin(reg.get(rt, rt), num_bits=5)
+        result += int2bin(reg.get(rd, rd), num_bits=5)
+        result += "00000"
+
+    result += int2bin(funct[ins], num_bits=6)
     return result
 
 
@@ -95,12 +93,14 @@ def make_i(ins, args, current_line, labels):
             "lw": 0x23,
             "sw": 0x2b
             }
-    result = ""
-    result += bin(opcode[ins])[2:].rjust(6, '0')
 
+    # get opcode
+    result = int2bin(opcode[ins], num_bits=6)
+
+    # args can be label, so get(rx, rx) is necessary
     rs, rt, imm = args
-    result += bin(reg.get(rt, rt))[2:].rjust(5, '0')
-    result += bin(reg.get(rs, rs))[2:].rjust(5, '0')
+    result += int2bin(reg.get(rt, rt), num_bits=5)
+    result += int2bin(reg.get(rs, rs), num_bits=5)
 
     if imm not in labels:
         result += twos_comp(int(imm), 16)
@@ -119,12 +119,23 @@ def make_j(ins, args, labels):
             "j": 0x2,
             "jal": 0x3
             }
-    result = bin(opcode[ins])[2:].rjust(6, "0")
+    # get opcode
+    result = int2bin(opcode[ins], num_bits=6)
+
+    # check if argument is a label
     target, = args
     if target in labels:
         target = labels[target]
-    result += bin(target)[2:].rjust(26, "0")
+
+    result += int2bin(target, num_bits=26)
     return result
+
+
+def int2bin(value, num_bits):
+    """
+    returns a num_bits-binary number represented as a string (w/o 0b)
+    """
+    return bin(int(value))[2:].rjust(num_bits, "0")
 
 
 def instruction_type(instruction):
@@ -137,10 +148,10 @@ def instruction_type(instruction):
     return None
 
 
-def twos_comp(val: int, bits):
+def twos_comp(val: int, num_bits):
     if val < 0:
-        val += 2**bits
-    return bin(val)[2:].rjust(bits, "0")
+        val += 2**num_bits
+    return bin(val)[2:].rjust(num_bits, "0")
 
 
 if __name__ == "__main__":
